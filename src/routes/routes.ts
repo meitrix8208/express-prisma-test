8b67eccdf81b891ae951c1862090d3aef0d9434b
meitrix8208 <maurrod2001@outlook.com>
2023-02-11 12:52:44 -0500

    update routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,39 @@
 import { Router } from "express";
-import { getUsers } from "../services/controllers";
+import {
+    getUser,
+    getUsers,
+    createUser,
+    updateUser,
+    deleteUser,
+    getPost,
+    getPosts,
+    createPost,
+    updatePost,
+    deletePost,
+    getProfile,
+    getProfiles,
+    createProfile,
+    updateProfile,
+    deleteProfile,
+} from "../services/controllers";
 const router = Router();
 
-router.get("/users",getUsers );
+router.post("/users", createUser);
+router.get("/users", getUsers);
+router.get("/users/:id", getUser);
+router.put("/users/:id", updateUser);
+router.delete("/users/:id", deleteUser);
 
+router.get("/posts", getPosts);
+router.get("/posts/:id", getPost);
+router.post("/posts", createPost);
+router.put("/posts/:id", updatePost);
+router.delete("/posts/:id", deletePost);
+
+router.get("/profile", getProfile);
+router.get("/profile/:id", getProfiles);
+router.post("/profile", createProfile);
+router.put("/profile/:id", updateProfile);
+router.delete("/profile/:id", deleteProfile);
 
 export default router;
